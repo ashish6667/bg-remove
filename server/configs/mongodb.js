ed10864d11d725b9f05418974bef510c55e4aa91
ashish6667 <ay1060045@gmail.com>
2025-04-22 17:26:13 +0530

    fix(mongodb): correct formatting and structure of database connection code fix(userModel): fix typo in model reference for user schema

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => {
-        console.log('Database connected ')
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal` )
-
-}
-
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected ");
+  });
+  await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+};
 
 export default connectDB;
