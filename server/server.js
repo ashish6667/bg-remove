3243883af0524c6105c5bb75846dd9fcd5c862c7
ashish6667 <ay1060045@gmail.com>
2025-04-22 23:36:46 +0530

    refactor(server): comment out MongoDB connection and simplify API response

--- server/server.js
+++ server/server.js
@@ -12,9 +12,8 @@ await connectDB()
 app.use(express.json())
 app.use(cors())
 // API routes
-app.get('/', (req, res) => {
-  res.send("API Working");
-});
+app.get('/', (req, res) => 
+  res.send("API Working"))
 
 
 app.listen(PORT, () => 
