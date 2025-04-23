f66fa43a5f0a591c5e563eca1b56ed94ed139964
ashish6667 <ay1060045@gmail.com>
2025-04-23 11:31:28 +0530

    fix(mongodb): remove unnecessary path from MongoDB connection URI

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on('connected', () => {
     console.log("Database connected ");
   })
-  await mongoose.connect(`${process.env.MONGODB_URI}/bg-remove`)
+  await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 
 export default connectDB;
