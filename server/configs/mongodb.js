ee495f6904ec452ee232ab9030b611d7734f7b04
ashish6667 <ay1060045@gmail.com>
2025-04-23 12:07:40 +0530

    fix(mongodb): update connection URI to include database name

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on('connected', () => {
+  mongoose.connection.on('connected', ()=>{
     console.log("Database connected ");
+
   })
-  await mongoose.connect(`${process.env.MONGODB_URI}`)
+  
+  await mongoose.connect(`${process.env.MONGODB_URI}/bg-remove`)
+
 }
 
-export default connectDB;
+export default connectDB
